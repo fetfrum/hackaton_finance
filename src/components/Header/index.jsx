37a4489a12a3a3d1005f8028fc02f95beb069a80
Igor Kruglenko <fetfrum@gmail.com>
2018-03-31 20:53:55 +0300

    c

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import '../Header/style.css';
+import './style.css';
 
-const Header = () => {
+const Header = (prop100, prop1500) => {
   return (
-    <div className="Header col s12">
-      <span className="App-header">HEADER</span>
+    <div className="Header blue darken-3">
+      
+    	<div className="row">
+    		<div className="col s6"></div>
+    	<div className="col s6">
+        	<div className="vallign wrapper right blue darken-3 right-align">
+        		<i className="h-icon material-icons blue darken-3 white-text">tv</i>
+        		<span className="blue darken-3 valign-wrapper white-text">{100 +" грн"}</span>
+        		<span className="blue darken-3 valign-wrapper white-text">{"/"+ 1500 +" грн"}</span>
+        	</div>
+      		</div>
+  		</div>
     </div>
   );
 };
--- src/components/LeftNav/index.jsx
+++ src/components/LeftNav/index.jsx
@@ -4,8 +4,23 @@ import './style.css';
 const LeftNav = () => {
   return (
     <div className="LeftNav col s3">
-      <span>LEFT_NAV</span>
-    </div>
+      <span><div class="container">
+          <div class="sidebar">
+            <ul>
+              <li><a href="#">Расходи</a></li>
+              <li><a href="#">Сегодня</a></li>
+            </ul>
+              <button class="but">
+                <ul>
+                  <li><a href="#">Статистика</a></li>
+                </ul>
+              </button>
+            
+          </div>
+        <div class="main-content">
+      </div>
+    </div></span>
+  </div>
   );
 };
 
